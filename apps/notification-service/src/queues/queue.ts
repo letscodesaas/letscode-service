e5a2cbd1a456873ea450bd1bdcec65fbcf626f50
shu12388y <spl.sp999@gmail.com>
2026-04-21 20:51:47 +0530

    fix the docker build

--- apps/notification-service/src/queues/queue.ts
+++
@@ -1,9 +0,0 @@
-import {QueueInstance} from "@letscode/queues/queues";
-export const queue = async(qname:string,jobname:string, data:any)=>{
-    try {
-        const q = new QueueInstance("",qname);
-        await q.addJob(jobname,data);
-    } catch (error) {
-        throw new Error(String(error))
-    }
-}
