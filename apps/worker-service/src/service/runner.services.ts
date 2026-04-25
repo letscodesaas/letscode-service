cb091e1e44be28f34d82eabb3108c61d8fb6b913
shu12388y <spl.sp999@gmail.com>
2026-04-25 19:39:04 +0530

    added batch processing

--- apps/worker-service/src/service/runner.services.ts
+++ apps/worker-service/src/service/runner.services.ts
@@ -8,10 +8,6 @@ const mailTrap = new MailTrapService(
   parseInt(ENV.ACCOUNT_ID) as number,
 );
 
-const delay = async () => {
-  return new Promise((r) => setTimeout(r, 2000));
-};
-
 export const runner = async (job: any) => {
   try {
     const sender = {
@@ -20,30 +16,37 @@ export const runner = async (job: any) => {
     };
     await connection(ENV.DB_URI);
     if (job.data.data) {
-      const email = await job?.data?.data?.email?.email;
+      const email = await job?.data?.data?.email;
       if (!email) return;
       const topic = await job?.data?.data?.topic;
       const html = await job?.data?.data?.html;
       const category = await job?.data?.data?.category;
       const subject = await job?.data?.data?.subject;
       const topic_stamp = await job?.data?.data?.topic_stamp;
+      const MAX = 500;
+      let e = [];
+      let START = 0;
+      let STOP = 0;
+      let COUNT = 0;
 
-      await delay();
-      const message = await mailTrap.sendBulkMails({
-        from: sender,
-        // @ts-ignore
-        to: [{ email: email }],
-        category: category,
-        subject: subject,
-        html: html,
-      });
-      if (message.success) {
-        await NotificationStatus.create({
-          topic: topic_stamp,
-          email: email,
-          status: "DONE",
-        });
-        console.log(message.success);
+      while (COUNT < email.length) {
+        if (email.length < MAX) {
+          let v = email.slice(START, email.length + 1);
+          e = [...v];
+          const info = await mailTrap.sendBatch(sender.email,subject,html,e);
+          console.log(info)
+          break;
+        } else {
+          STOP = STOP + MAX;
+          let v = email.slice(START, STOP);
+          e = [...v];
+          await new Promise((r) => setTimeout(r, 5000));
+          const info = await mailTrap.sendBatch(sender.email,subject,html,e);
+          console.log(info)
+          START = STOP;
+          COUNT += MAX;
+          e = [];
+        }
       }
     }
   } catch (error) {
