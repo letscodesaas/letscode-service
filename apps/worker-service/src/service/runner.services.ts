d6695809dbb92c2c8c128f097f1d5a11460a47e6
shu12388y <spl.sp999@gmail.com>
2026-04-29 18:26:31 +0530

    added aws ses

--- apps/worker-service/src/service/runner.services.ts
+++ apps/worker-service/src/service/runner.services.ts
@@ -1,51 +1,82 @@
-import { MailTrapService } from "@letscode/services/service";
+import { SES } from "@letscode/services/service";
 import { connection, NotificationStatus } from "@letscode/databases/models";
 import { ENV } from "../env/env.js";
 
-const mailTrap = new MailTrapService(
-  ENV.MAILTRAP_TOKEN,
-  true,
-  parseInt(ENV.ACCOUNT_ID) as number,
-);
+const sesClient = new SES(ENV.ACCESS_KEY, ENV.SECRET_KEY);
 
 export const runner = async (job: any) => {
   try {
     const sender = {
-      email: "hello@www.lets-code.co.in",
+      email: "letscode@lets-code.co.in",
       name: "Lets Code",
     };
+
     await connection(ENV.DB_URI);
     if (job.data.data) {
       const email = await job?.data?.data?.email;
       if (!email) return;
-      const topic = await job?.data?.data?.topic;
       const html = await job?.data?.data?.html;
-      const category = await job?.data?.data?.category;
       const subject = await job?.data?.data?.subject;
-      const topic_stamp = await job?.data?.data?.topic_stamp;
-      const MAX = 500;
+      const MAX = 14;
       let e = [];
       let START = 0;
       let STOP = 0;
       let COUNT = 0;
-
       while (COUNT < email.length) {
         if (email.length < MAX) {
           let v = email.slice(START, email.length + 1);
           e = [...v];
-          const info = await mailTrap.sendBatch(sender.email,subject,html,e);
-          console.log(info)
+          for (const emails of e) {
+            const info = await sesClient.sendMails({
+              Source: sender.email,
+              Destination: { ToAddresses: [emails] },
+              Message: {
+                Subject: {
+                  Data: subject,
+                },
+                Body: {
+                  Html: { Data: html },
+                },
+              },
+            });
+            await NotificationStatus.create({
+              topic: info.MessageId,
+              email: emails,
+              status: info.$metadata.httpStatusCode,
+            });
+            console.log(info);
+            await new Promise((r)=>setTimeout(r,2000))
+          }
           break;
         } else {
           STOP = STOP + MAX;
           let v = email.slice(START, STOP);
           e = [...v];
-          await new Promise((r) => setTimeout(r, 5000));
-          const info = await mailTrap.sendBatch(sender.email,subject,html,e);
-          console.log(info)
+          for (const emails of e) {
+            const info = await sesClient.sendMails({
+              Source: sender.email,
+              Destination: { ToAddresses: emails },
+              Message: {
+                Subject: {
+                  Data: subject,
+                },
+                Body: {
+                  Html: { Data: html },
+                },
+              },
+            });
+            await NotificationStatus.create({
+              topic: info.MessageId,
+              email: emails,
+              status: info.$metadata.httpStatusCode,
+            });
+            console.log(info);
+            await new Promise((r)=>setTimeout(r,2000))
+          }
           START = STOP;
           COUNT += MAX;
           e = [];
+          await new Promise((r) => setTimeout(r, 2000));
         }
       }
     }
